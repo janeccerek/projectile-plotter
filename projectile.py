376454d69420dfe64777a18f751bc89bbec8797b
jnowosielski <34610972+janeccerek@users.noreply.github.com>
2021-04-13 16:47:57 +0200

    Komentarze i fix

--- projectile.py
+++ projectile.py
@@ -3,69 +3,110 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-def calculate_projectile():
-    dt = 0.01
+def get_data():
+    # funkcja prosi użytkownika o podanie danych i potem wpisuje je do słownika
+    print("Witaj w programie rysującym trajektorię ciała.\nPodaj kolejno oddzielając przecinkiem:")
+    data = {}
+    data.update({"mass": float(input("masę ciała [kg]: "))})
+    data.update({"height": float(input("wysokość początkową [m]: "))})
+    data.update({"res": float(input("współczynnik oporu powietrza [kg/m]: "))})
+    data.update({"theta": float(input("kąt pod jakim wyrzucono ciało [stopnie]: "))})
+    data.update({"velocity": float(input("prędkość początkową [m/s]: "))})
+
+    return data
+
+
+def calculate_projectile(data, dt):
+    # funkcja wyznacza trajektorię ciała dla podanych danych
+    # wczytanie stałych i wartości początkowych
     g = 9.81
-    print("Podaj kolejno oddzielając przecinkiem: \n")
-    m = float(input("masę ciała [kg]: "))
-    h = float(input("wysokość początkową [m]: "))
-    B = float(input("współczynnik oporu powietrza [kg/m]: "))
-    theta = float(input("kąt pod jakim wyrzucono ciało [stopnie]: "))
-    v0 = float(input("prędkość początkową [m/s]: "))
+    m = data["mass"]
+    h = data["height"]
+    B = data["res"]
+    theta = data["theta"]
+    v0 = data["velocity"]
+    v = v0
     v_x = v0 * np.cos(np.radians(theta))
     v_y = v0 * np.sin(np.radians(theta))
     pos = [[0], [h]]
-    iter = 0
+    ii = 1
     x = 0
     y = h
-    while y >= 0.0:
-        force_x = B * v_x ** 2
-        force_y = g + np.sign(v_y) * B * v_y ** 2
-        x = x + v_x * dt - (force_x / (2 * m)) * dt ** 2
-        y = y + v_y * dt - (force_y / (2 * m)) * dt ** 2
+
+    # korzystam z metody Eulera do numerycznego rozwiązania równania różniczkowego
+    while pos[1][ii-1] >= 0.0:
+        force_x = B * v_x * v
+        force_y = g + B * v_y * v
+        x = x + v_x * dt
+        y = y + v_y * dt
         v_x = v_x - (force_x / m) * dt
         v_y = v_y - (force_y / m) * dt
+        v = np.sqrt(v_y**2 + v_x**2)
         pos[0].append(x)
         pos[1].append(y)
-        iter += 1
+        ii += 1
 
-    pos[0].append(x + v_x * dt - (force_x / (2 * m)) * dt ** 2)
-    pos[1].append(0)
-    iter += 1
-    return pos, iter
+    return pos
 
 
-pos_list, numframes = calculate_projectile()
+def optimize_interval(data):
+    # Funkcja ma za zadanie zoptymalizować używane przy obliczaniu trajektorii dt tak, żeby animacja rysowała się
+    # w sensownym czasie. Bez niej dla dużych wartości początkowych wykres rysuje się już niewygodnie długo.
+    # Wpływa to na dokładność wyliczeń, ponieważ interwał dt zmienia się i czas spadku z tej samej wysokości dla
+    # różnych wartości początkowych może się nieznacznie różnić. Co ważne, nie jest to duży rozrzut.
+    dt = 0.01
+    for i in range(5):
+        pos = calculate_projectile(data, dt)
+        if len(pos[1]) < 400:
+            dt /= 2
+        elif len(pos[1]) > 1000:
+            dt *= 2
+        else:
+            return dt
+
+    return dt
 
 
+# wywołuję globalnie wartość interwału, dane i listę kolejnych współrzędnych ciała
+data = get_data()
+interval = optimize_interval(data)
+pos_list = calculate_projectile(data, interval)
+
+# deklaruję elementy wykresu wykorzystywane później do animacji: line jest krzywą zakreśloną przez ciało, point to
+# marker punktu, w którym w danej chwili znajduje się ciało, a time_text to licznik czasu
 fig = plt.subplots()
-plt.xlim(0, 1.2*max(pos_list[0]))
+plt.xlim(1.2*min(pos_list[0]), 1.2*max(pos_list[0]))
 plt.ylim(0, 1.2*max(pos_list[1]))
 plt.xlabel("współrzędna x [m]")
 plt.ylabel("współrzędna y [m]")
+plt.title("rzut poziomy z oporem w zależności od $V^2$")
 line, = plt.plot([], [], color="green")
-point, = plt.plot([], [], marker = "o", color="green")
-time_text  = plt.text(0.02, 0.95, '')
+point, = plt.plot([], [], marker="o", color="green")
+time_text = plt.annotate(0, xy=(1, 1), xytext=(1, 1))
 xdata, ydata = [], []
 
 
 def init():
-    line.set_data([],[])
-    point.set_data([],[])
-    return line,
+    # inicjalizuję elementy wykresu
+    line.set_data([], [])
+    point.set_data([], [])
+    time_text.set_text("")
+    return line, point, time_text
 
 
 def animate(i):
+    # funkcja aktualizująca wykres poprzez dodawanie kolejnych punktów oraz aktualizowanie licznika czasu
     xdata.append(pos_list[0][i])
     ydata.append(pos_list[1][i])
     line.set_data(xdata, ydata)
     point.set_data(pos_list[0][i], pos_list[1][i])
-    t = 0.01 * i
-    time_text.set_text("czas: {:.2f}s".format(t))
+    t = interval * i
+    t = round(t, 2)
+    time_text.set_text("czas: " + str(t) + "s")
     return line, point, time_text
 
 
-anim = animation.FuncAnimation(fig[0], animate, init_func=init,
-							frames=numframes, interval=10, blit=True,
-                            repeat=False)
+# wywołanie animacji
+anim = animation.FuncAnimation(fig[0], animate, init_func=init, frames=len(pos_list[1]), interval=20/len(pos_list[1]),
+                               blit=True, repeat=False)
 plt.show()
